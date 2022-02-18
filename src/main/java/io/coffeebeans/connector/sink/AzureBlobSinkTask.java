1c9d50ee5c8f821d8b6ac3d8c79f279bb0eaf270
vishal bihani <vishal@coffeebeans.io>
2022-02-18 11:25:36 +0530

    Store received json to blob storage

--- src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
+++ src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
@@ -1,5 +1,7 @@
 package io.coffeebeans.connector.sink;
 
+import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import io.coffeebeans.connector.sink.storage.AzureBlobStorageManager;
 import io.coffeebeans.connector.sink.util.Version;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
@@ -11,6 +13,9 @@ import java.util.Map;
 
 public class AzureBlobSinkTask extends SinkTask {
     private static final Logger logger = LoggerFactory.getLogger(AzureBlobSinkTask.class);
+    private AzureBlobSinkConfig config;
+    private AzureBlobStorageManager storageManager;
+    private String containerName;
 
     @Override
     public String version() {
@@ -18,14 +23,22 @@ public class AzureBlobSinkTask extends SinkTask {
     }
 
     @Override
-    public void start(Map<String, String> map) {
+    public void start(Map<String, String> config) {
         logger.info("Starting Sink Task ....................");
+        this.config = new AzureBlobSinkConfig(config);
+
+        String connectionString = this.config.getConnectionString();
+        this.containerName = this.config.getContainerName();
+        logger.info("Connection String: {}", connectionString);
+        logger.info("Container Name: {}", this.containerName);
+        this.storageManager = new AzureBlobStorageManager(connectionString);
     }
 
     @Override
     public void put(Collection<SinkRecord> collection) {
         for (SinkRecord record : collection) {
             logger.info("Task record value: " + record.value());
+            this.storageManager.upload(this.containerName, record.value());
         }
     }
 
