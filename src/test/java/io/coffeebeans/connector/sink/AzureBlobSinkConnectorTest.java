a14b8d5ff2321d192769b7a626f1bf69d9a07c6e
vishal bihani <vishal@coffeebeans.io>
2022-02-21 11:18:40 +0530

    Junit test for sink connector

---
+++ src/test/java/io/coffeebeans/connector/sink/AzureBlobSinkConnectorTest.java
@@ -0,0 +1,21 @@
+package io.coffeebeans.connector.sink;
+
+import org.apache.kafka.connect.connector.Connector;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AzureBlobSinkConnectorTest {
+
+    @Test
+    public void testVersion() {
+        String version = new AzureBlobSinkConnector().version();
+        Assertions.assertNotNull(version);
+        Assertions.assertFalse(version.isEmpty());
+    }
+
+    @Test
+    public void connectorType() {
+        Connector connector = new AzureBlobSinkConnector();
+        Assertions.assertTrue(AzureBlobSinkConnector.class.isAssignableFrom(connector.getClass()));
+    }
+}
