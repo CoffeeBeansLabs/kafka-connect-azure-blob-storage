32241dd7ab3c2a1714a89f1c27d45a6541807d04
vishal bihani <vishal@coffeebeans.io>
2022-02-24 18:30:51 +0530

    Added unit test for connection string validator

---
+++ src/test/java/io/coffeebeans/connector/sink/config/validators/ConnectionStringValidatorTest.java
@@ -0,0 +1,53 @@
+package io.coffeebeans.connector.sink.config.validators;
+
+import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import org.apache.kafka.common.config.ConfigException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConnectionStringValidatorTest {
+    private static final String CONNECTION_STRING_NULL = null;
+    private static final String CONNECTION_STRING_EMPTY = "";
+    private static final String CONNECTION_STRING_BLANK = "     " ;
+    private static final String CONNECTION_STRING_VALID = "AccountName=devstoreaccount1;" +
+            "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K" +
+            "1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://" +
+            "host.docker.internal:10000/devstoreaccount1;";
+
+
+    @Test
+    public void test_nullConnectionString() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ConnectionStringValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONN_STRING_CONF, CONNECTION_STRING_NULL
+            );
+        }, "Invalid connection string: ");
+    }
+
+    @Test
+    public void test_emptyConnectionString() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ConnectionStringValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONN_STRING_CONF, CONNECTION_STRING_EMPTY
+            );
+        }, "Invalid connection string: ");
+    }
+
+    @Test
+    public void test_blankConnectionString() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ConnectionStringValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONN_STRING_CONF, CONNECTION_STRING_BLANK
+            );
+        }, "Invalid connection string: ");
+    }
+
+    @Test
+    public void test_validConnectionString() {
+        Assertions.assertDoesNotThrow(() -> {
+            new ConnectionStringValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONN_STRING_CONF, CONNECTION_STRING_VALID
+            );
+        });
+    }
+}
