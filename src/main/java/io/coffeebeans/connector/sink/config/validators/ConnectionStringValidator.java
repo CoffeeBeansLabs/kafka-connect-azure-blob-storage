600b62afbe798c13d3651cafe14d6153797fb97a
vishal bihani <vishal@coffeebeans.io>
2022-02-24 18:27:56 +0530

    Added validator for connection string

---
+++ src/main/java/io/coffeebeans/connector/sink/config/validators/ConnectionStringValidator.java
@@ -0,0 +1,29 @@
+package io.coffeebeans.connector.sink.config.validators;
+
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
+
+public class ConnectionStringValidator implements ConfigDef.Validator {
+
+    /**
+     * Ensure the provided connection string is not null, empty, blank and malformed.
+     * @param configurationKey Configuration key
+     * @param configurationValue Connection String
+     */
+    @Override
+    public void ensureValid(String configurationKey, Object configurationValue) {
+        String connectionString = (String) configurationValue;
+
+        try {
+
+            // If the connection string is malformed or incorrect the azure sdk will throw an exception
+            new BlobServiceClientBuilder()
+                    .connectionString(connectionString)
+                    .buildClient();
+
+        } catch (IllegalArgumentException e) {
+            throw new ConfigException("Invalid connection string: ", connectionString);
+        }
+    }
+}
