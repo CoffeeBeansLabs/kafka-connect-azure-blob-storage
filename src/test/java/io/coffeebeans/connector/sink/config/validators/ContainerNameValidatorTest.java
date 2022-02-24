3bca6b8f4fddc316f47bf599aef9d988e15cb402
vishal bihani <vishal@coffeebeans.io>
2022-02-24 18:56:21 +0530

    Added unit tests for container name validator

---
+++ src/test/java/io/coffeebeans/connector/sink/config/validators/ContainerNameValidatorTest.java
@@ -0,0 +1,49 @@
+package io.coffeebeans.connector.sink.config.validators;
+
+import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import org.apache.kafka.common.config.ConfigException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ContainerNameValidatorTest {
+    private static final String CONTAINER_NAME_NULL = null;
+    private static final String CONTAINER_NAME_EMPTY = "";
+    private static final String CONTAINER_NAME_BLANK = "     ";
+    private static final String CONTAINER_NAME_VALID = "test";
+
+    @Test
+    public void test_nullContainerName() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ContainerNameValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONTAINER_NAME_CONF, CONTAINER_NAME_NULL
+            );
+        }, "Invalid container name: ");
+    }
+
+    @Test
+    public void test_emptyContainerName() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ContainerNameValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONTAINER_NAME_CONF, CONTAINER_NAME_EMPTY
+            );
+        }, "Invalid container name: ");
+    }
+
+    @Test
+    public void test_blankContainerName() {
+        Assertions.assertThrowsExactly(ConfigException.class, () -> {
+            new ContainerNameValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONTAINER_NAME_CONF, CONTAINER_NAME_BLANK
+            );
+        }, "Invalid container name: ");
+    }
+
+    @Test
+    public void test_validContainerName() {
+        Assertions.assertDoesNotThrow(() -> {
+            new ContainerNameValidator().ensureValid(
+                    AzureBlobSinkConfig.AZURE_BLOB_CONTAINER_NAME_CONF, CONTAINER_NAME_VALID
+            );
+        });
+    }
+}
