45d96e7b7b80279db75f947e3c99194bebc55530
vishal bihani <vishal@coffeebeans.io>
2022-03-03 10:08:10 +0530

    Added partitioner configurations

--- src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
+++ src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
@@ -3,6 +3,9 @@ package io.coffeebeans.connector.sink;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import io.coffeebeans.connector.sink.partitioner.DefaultPartitioner;
+import io.coffeebeans.connector.sink.partitioner.FieldPartitioner;
+import io.coffeebeans.connector.sink.partitioner.Partitioner;
 import io.coffeebeans.connector.sink.storage.AzureBlobStorageManager;
 import io.coffeebeans.connector.sink.util.StructToMap;
 import io.coffeebeans.connector.sink.util.Version;
@@ -17,9 +20,10 @@ import java.util.*;
 public class AzureBlobSinkTask extends SinkTask {
     private static final Logger logger = LoggerFactory.getLogger(AzureBlobSinkTask.class);
 
+    private long startingOffset;
     private String containerName;
+    private Partitioner partitioner;
     private String blobIdentifierKey;
-    private String containerIdentifier;
     private ObjectMapper objectMapper;
     private AzureBlobStorageManager storageManager;
 
@@ -38,6 +42,8 @@ public class AzureBlobSinkTask extends SinkTask {
         objectMapper = new ObjectMapper();
         storageManager = new AzureBlobStorageManager(config.getConnectionString());
 
+        setPartitioner(configProps);
+
         logger.debug("Blob identifier key: {}", this.blobIdentifierKey);
         logger.debug("Container name: {}", this.containerName);
         logger.debug("Connection url: {}", config.getConnectionString());
@@ -45,12 +51,17 @@ public class AzureBlobSinkTask extends SinkTask {
 
     @Override
     public void put(Collection<SinkRecord> collection) {
+        if (collection.isEmpty()) return;
+
         List<SinkRecord> records = new ArrayList<>(collection);
         logger.debug("Received {} records", records.size());
 
+        // Storing the starting offset if not present
+        setStartingOffset(records.get(0).kafkaOffset());
+
         // Loop through each record and store it in the blob storage.
         for (SinkRecord record : records) {
-            logger.info("Task record value: " + record.value());
+            logger.debug("Task record value: " + record.value());
 
             Map<?, ?> valueMap;
 
@@ -70,7 +81,9 @@ public class AzureBlobSinkTask extends SinkTask {
             if (valueMap.isEmpty()) return;
 
             // Get the blob name using the identifier key
-            String blobName = (String) valueMap.get(blobIdentifierKey);
+            // String blobName = (String) valueMap.get(blobIdentifierKey);
+            String blobName = partitioner.encodePartition(record, startingOffset);
+            logger.debug("blob name: " + blobName);
 
             byte[] data;
             try {
@@ -92,6 +105,20 @@ public class AzureBlobSinkTask extends SinkTask {
 
     private byte[] getValueAsBytes(Map<?, ?> valueMap) throws JsonProcessingException {
         return objectMapper.writeValueAsBytes(valueMap);
+    }
+
+    private void setPartitioner(Map<String, String> configProps) {
+        switch (configProps.get(AzureBlobSinkConfig.PARTITION_STRATEGY_CONF)) {
+            case "FIELD": partitioner = new FieldPartitioner();
+            break;
+
+            default: partitioner = new DefaultPartitioner();
+        }
+        partitioner.configure(configProps);
+    }
+
+    private void setStartingOffset(long offset) {
+        if (startingOffset == 0) startingOffset = offset;
     }
 
 }
