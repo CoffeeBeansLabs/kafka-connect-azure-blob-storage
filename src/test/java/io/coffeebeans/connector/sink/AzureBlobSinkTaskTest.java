13689e3929d3de7950c9a67aebc88f8f60a599a5
vishal bihani <vishal@coffeebeans.io>
2022-03-03 10:09:07 +0530

    Added partitioner to tests

--- src/test/java/io/coffeebeans/connector/sink/AzureBlobSinkTaskTest.java
+++ src/test/java/io/coffeebeans/connector/sink/AzureBlobSinkTaskTest.java
@@ -2,13 +2,15 @@ package io.coffeebeans.connector.sink;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import io.coffeebeans.connector.sink.partitioner.DefaultPartitioner;
+import io.coffeebeans.connector.sink.partitioner.Partitioner;
 import io.coffeebeans.connector.sink.storage.AzureBlobStorageManager;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.kafka.connect.data.ConnectSchema;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -17,9 +19,32 @@ import java.util.List;
 import java.util.Map;
 
 public class AzureBlobSinkTaskTest {
-    private final String topicName = "test";
-    private final String containerName = "test-container";
-    private final String blobIdentifierKey = "id";
+    // CONFIGURATION PROPS
+    private static final String CONNECTION_STRING = "AccountName=devstoreaccount1;" +
+            "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K" +
+            "1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://" +
+            "host.docker.internal:10000/devstoreaccount1;";
+    private static final String CONTAINER_NAME = "containerName";
+    private static final String BLOB_IDENTIFIER_KEY = "blobIdentifierKey";
+    private static final String TOPIC_DIR = "test";
+    private static final String PARTITION_STRATEGY = "DEFAULT";
+
+    // FIELD NAMES
+    private static final String FIELD_STARTING_OFFSET = "startingOffset";
+    private static final String FIELD_PARTITIONER = "partitioner";
+    private static final String FIELD_OBJECT_MAPPER = "objectMapper";
+    private static final String FIELD_STORAGE_MANAGER = "storageManager";
+
+    // FIELD VALUES
+    private static final String STARTING_OFFSET = "0";
+    private static final Partitioner PARTITIONER = new DefaultPartitioner();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    // KAFKA PROPERTIES
+    private static final String KAFKA_TOPIC_NAME = "test";
+    private static final int KAFKA_PARTITION = 0;
+    private static final String KAFKA_RECORD_KEY = "nokey";
+    private static final long KAFKA_OFFSET = 0L;
 
     private AzureBlobStorageManager mockStorageManager;
     private AzureBlobSinkTask sinkTask;
@@ -29,21 +54,42 @@ public class AzureBlobSinkTaskTest {
         sinkTask = new AzureBlobSinkTask();
         mockStorageManager = Mockito.mock(AzureBlobStorageManager.class);
 
-        FieldUtils.writeField(sinkTask, "containerName", containerName, true);
-        FieldUtils.writeField(sinkTask, "blobIdentifierKey", blobIdentifierKey, true);
-        FieldUtils.writeField(sinkTask, "objectMapper", new ObjectMapper(), true);
-        FieldUtils.writeField(sinkTask, "storageManager", mockStorageManager, true);
+        Map<String, String> configProps = new HashMap<>();
+        configProps.put(AzureBlobSinkConfig.CONN_STRING_CONF, CONNECTION_STRING);
+        configProps.put(AzureBlobSinkConfig.CONTAINER_NAME_CONF, CONTAINER_NAME);
+        configProps.put(AzureBlobSinkConfig.BLOB_IDENTIFIER_KEY, BLOB_IDENTIFIER_KEY);
+        configProps.put(AzureBlobSinkConfig.TOPIC_DIR, TOPIC_DIR);
+        configProps.put(AzureBlobSinkConfig.PARTITION_STRATEGY_CONF, PARTITION_STRATEGY);
+
+        sinkTask.start(configProps);
+
+        FieldUtils.writeField(sinkTask, FIELD_PARTITIONER, PARTITIONER, true);
+        FieldUtils.writeField(sinkTask, FIELD_OBJECT_MAPPER, OBJECT_MAPPER, true);
+        FieldUtils.writeField(sinkTask, FIELD_STORAGE_MANAGER, mockStorageManager, true);
     }
 
     @Test
     public void test_putString_shouldNotProcess() throws IllegalAccessException {
-        init();
+        init(); // Initialize SinkTask
         Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
-
         String message = "message";
-        SinkRecord sinkRecord = new SinkRecord(topicName, 1, stringSchema, "nokey", stringSchema, message, 0L);
+
+        // Create sink record
+        SinkRecord sinkRecord = new SinkRecord(
+                KAFKA_TOPIC_NAME,
+                KAFKA_PARTITION,
+                stringSchema,
+                KAFKA_RECORD_KEY,
+                stringSchema,
+                message,
+                KAFKA_OFFSET);
+
+        // Assert sink record is not null
         Assertions.assertNotNull(sinkRecord.value());
+
         sinkTask.put(List.of(sinkRecord));
+
+        // Verify Storage manager is not invoked
         Mockito.verify(mockStorageManager, Mockito.times(0)).upload(
                 Mockito.any(), Mockito.any(), Mockito.any()
         );
@@ -52,6 +98,7 @@ public class AzureBlobSinkTaskTest {
     @Test
     public void test_putMap_shouldProcess() throws JsonProcessingException, IllegalAccessException {
         init();
+        Partitioner partitioner = new DefaultPartitioner();
         Schema stringSchema = new ConnectSchema(Schema.Type.STRING);
         Schema mapSchema = new ConnectSchema(Schema.Type.MAP);
 
@@ -59,11 +106,23 @@ public class AzureBlobSinkTaskTest {
         map.put("id", "test-blob");
         map.put("key", "value");
 
-        SinkRecord sinkRecord = new SinkRecord(topicName, 1, stringSchema, "nokey", mapSchema, map, 0L);
+        SinkRecord sinkRecord = new SinkRecord(
+                KAFKA_TOPIC_NAME,
+                KAFKA_PARTITION,
+                stringSchema,
+                KAFKA_RECORD_KEY,
+                mapSchema,
+                map,
+                KAFKA_OFFSET);
+
         Assertions.assertNotNull(sinkRecord.value());
+
         sinkTask.put(List.of(sinkRecord));
+
+        String expectedBlobName = partitioner.encodePartition(sinkRecord, KAFKA_OFFSET);
+
         Mockito.verify(mockStorageManager, Mockito.times(1)).upload(
-                containerName, "test-blob", new ObjectMapper().writeValueAsBytes(sinkRecord.value())
+                CONTAINER_NAME, expectedBlobName, OBJECT_MAPPER.writeValueAsBytes(sinkRecord.value())
         );
     }
 }
