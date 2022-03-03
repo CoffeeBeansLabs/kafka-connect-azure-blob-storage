f04f557fdf287e8bdf9d9efa27ac9986939cfcab
vishal bihani <vishal@coffeebeans.io>
2022-03-03 10:07:49 +0530

    Added partitioner configurations

--- src/main/java/io/coffeebeans/connector/sink/config/AzureBlobSinkConfig.java
+++ src/main/java/io/coffeebeans/connector/sink/config/AzureBlobSinkConfig.java
@@ -26,6 +26,19 @@ public class AzureBlobSinkConfig extends AbstractConfig {
     public static final String BLOB_IDENTIFIER_KEY_DEFAULT = "blob";
     public static final String BLOB_IDENTIFIER_KEY_DOC = "Key to identify blob";
 
+    // Partitioner related configurations
+    public static final String TOPIC_DIR = "topic.dir";
+    public static final String TOPIC_DIR_DOC = "Parent directory where data from this topic will be stored";
+
+    public static final String PARTITION_STRATEGY_CONF = "partition.strategy";
+    public static final String PARTITION_STRATEGY_DEFAULT = "DEFAULT";
+    public static final String PARTITION_STRATEGY_DOC = "Partition strategy to be used";
+
+    // Field based partitioning configurations
+    public static final String PARTITION_STRATEGY_FIELD_CONF = "partition.strategy.field";
+    public static final String PARTITION_STRATEGY_FIELD_DEFAULT = null;
+    public static final String PARTITION_STRATEGY_FIELD_DOC = "Field name for field-based partitioning";
+
     // Common validators
     public static final Validator NON_EMPTY_STRING_VALIDATOR = new ConfigDef.NonEmptyString();
 
@@ -34,6 +47,9 @@ public class AzureBlobSinkConfig extends AbstractConfig {
     private final String connectionString;
     private final String containerName;
     private final String blobIdentifier;
+    private final String topicDir;
+    private final String partitionStrategy;
+    private final String fieldName;
 
     public AzureBlobSinkConfig(Map<String, String> parsedConfig) {
         this(getConfig(), parsedConfig);
@@ -44,6 +60,9 @@ public class AzureBlobSinkConfig extends AbstractConfig {
         this.connectionString = this.getString(CONN_STRING_CONF);
         this.containerName = this.getString(CONTAINER_NAME_CONF);
         this.blobIdentifier = this.getString(BLOB_IDENTIFIER_KEY);
+        this.topicDir = this.getString(TOPIC_DIR);
+        this.partitionStrategy = this.getString(PARTITION_STRATEGY_CONF);
+        this.fieldName = this.getString(PARTITION_STRATEGY_FIELD_CONF);
     }
 
 
@@ -69,16 +88,37 @@ public class AzureBlobSinkConfig extends AbstractConfig {
                         ConfigDef.Type.STRING,
                         CONTAINER_NAME_DEFAULT,
                         CONTAINER_NAME_VALIDATOR,
-                        ConfigDef.Importance.HIGH,
+                        ConfigDef.Importance.LOW,
                         CONTAINER_NAME_DOC
                 )
                 .define(
                         BLOB_IDENTIFIER_KEY,
                         ConfigDef.Type.STRING,
-                        BLOB_IDENTIFIER_KEY_DEFAULT,
+                        ConfigDef.Importance.LOW,
+                        BLOB_IDENTIFIER_KEY_DOC
+                )
+                .define( // Name of directory to store the records
+                        TOPIC_DIR,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.NO_DEFAULT_VALUE,
                         NON_EMPTY_STRING_VALIDATOR,
                         ConfigDef.Importance.MEDIUM,
-                        BLOB_IDENTIFIER_KEY_DOC
+                        TOPIC_DIR_DOC
+                )
+                .define( // Partition strategy configuration
+                        PARTITION_STRATEGY_CONF,
+                        ConfigDef.Type.STRING,
+                        PARTITION_STRATEGY_DEFAULT,
+                        NON_EMPTY_STRING_VALIDATOR,
+                        ConfigDef.Importance.MEDIUM,
+                        PARTITION_STRATEGY_DOC
+                )
+                .define( // Field-based partition strategy configuration
+                        PARTITION_STRATEGY_FIELD_CONF,
+                        ConfigDef.Type.STRING,
+                        PARTITION_STRATEGY_FIELD_DEFAULT,
+                        ConfigDef.Importance.MEDIUM,
+                        PARTITION_STRATEGY_FIELD_DOC
                 );
     }
 
@@ -92,5 +132,17 @@ public class AzureBlobSinkConfig extends AbstractConfig {
 
     public String getBlobIdentifier() {
         return this.blobIdentifier;
+    }
+
+    public String getTopicDir() {
+        return this.topicDir;
+    }
+
+    public String getPartitionStrategy() {
+        return this.partitionStrategy;
+    }
+
+    public String getFieldName() {
+        return this.fieldName;
     }
 }
