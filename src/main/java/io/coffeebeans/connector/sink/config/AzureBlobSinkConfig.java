e9286333f487c6eeff060f77497a1cac87cc125e
vishal bihani <vishal@coffeebeans.io>
2022-03-08 00:22:15 +0530

    Added Partitioner configs

--- src/main/java/io/coffeebeans/connector/sink/config/AzureBlobSinkConfig.java
+++ src/main/java/io/coffeebeans/connector/sink/config/AzureBlobSinkConfig.java
@@ -2,42 +2,67 @@ package io.coffeebeans.connector.sink.config;
 
 import io.coffeebeans.connector.sink.config.validators.ConnectionStringValidator;
 import io.coffeebeans.connector.sink.config.validators.ContainerNameValidator;
+import java.util.Map;
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Validator;
 
-import java.util.Map;
-
 public class AzureBlobSinkConfig extends AbstractConfig {
 
-    // Connection related configurations
+    /**
+     * Azure Blob Connection related configurations
+     */
+    // Connection String related configurations
     public static final String CONN_STRING_CONF = "connection.string";
     public static final Validator CONN_STRING_VALIDATOR = new ConnectionStringValidator();
     public static final String CONN_STRING_DOC = "Connection string of the azure blob storage";
 
-    // Container related configurations
+    /**
+     * Container configurations where blobs will be stored.
+     * If no value provided, a container with name 'default' will be created.
+     * ContainerNameValidator checks if the provided value is not null, empty and blank.
+     */
     public static final String CONTAINER_NAME_CONF = "container.name";
     public static final String CONTAINER_NAME_DEFAULT = "default";
     public static final Validator CONTAINER_NAME_VALIDATOR = new ContainerNameValidator();
-    public static final String CONTAINER_NAME_DOC = "Container name to store the blobs";
+    public static final String CONTAINER_NAME_DOC = "Name of the container where blobs will be stored";
 
     // Blob related configurations
     public static final String BLOB_IDENTIFIER_KEY = "blob.identifier.key";
     public static final String BLOB_IDENTIFIER_KEY_DEFAULT = "blob";
     public static final String BLOB_IDENTIFIER_KEY_DOC = "Key to identify blob";
 
-    // Partitioner related configurations
+    /**
+     * Parent directory where blobs will be stored.
+     */
     public static final String TOPIC_DIR = "topic.dir";
     public static final String TOPIC_DIR_DOC = "Parent directory where data from this topic will be stored";
 
+    /**
+     * Partition strategy configuration.
+     */
     public static final String PARTITION_STRATEGY_CONF = "partition.strategy";
     public static final String PARTITION_STRATEGY_DEFAULT = "DEFAULT";
     public static final String PARTITION_STRATEGY_DOC = "Partition strategy to be used";
 
-    // Field based partitioning configurations
+    // Field for partitioning configurations
     public static final String PARTITION_STRATEGY_FIELD_CONF = "partition.strategy.field";
     public static final String PARTITION_STRATEGY_FIELD_DEFAULT = null;
-    public static final String PARTITION_STRATEGY_FIELD_DOC = "Field name for field-based partitioning";
+    public static final String PARTITION_STRATEGY_FIELD_DOC = "Field name for partitioning";
+
+    // Time based partitioning configurations
+    public static final String PARTITION_STRATEGY_TIME_PATH_FORMAT_CONF = "path.format";
+    public static final String PARTITION_STRATEGY_TIME_PATH_FORMAT_DOC = "Output file path time partition format";
+
+    public static final String PARTITION_STRATEGY_TIME_BUCKET_MS_CONF = "partition.duration.ms";
+    public static final String PARTITION_STRATEGY_TIME_BUCKET_MS_DOC = "Time partition granularity";
+
+    // TODO: Default should be UTC
+    public static final String PARTITION_STRATEGY_TIME_TIMEZONE_CONF = "timezone";
+    public static final String PARTITION_STRATEGY_TIME_TIMEZONE_DOC = "Timezone for the time partitioner";
+
+    public static final String PARTITION_STRATEGY_TIME_EXTRACTOR_CONF = "timestamp.extractor";
+    public static final String PARTITION_STRATEGY_TIME_EXTRACTOR_DOC = "Time extractor for time based partitioner";
 
     // Common validators
     public static final Validator NON_EMPTY_STRING_VALIDATOR = new ConfigDef.NonEmptyString();
@@ -50,6 +75,11 @@ public class AzureBlobSinkConfig extends AbstractConfig {
     private final String topicDir;
     private final String partitionStrategy;
     private final String fieldName;
+    private final String pathFormat;
+    private final String timeBucket;
+    private final String timezone;
+    private final String timeExtractor;
+
 
     public AzureBlobSinkConfig(Map<String, String> parsedConfig) {
         this(getConfig(), parsedConfig);
@@ -63,6 +93,10 @@ public class AzureBlobSinkConfig extends AbstractConfig {
         this.topicDir = this.getString(TOPIC_DIR);
         this.partitionStrategy = this.getString(PARTITION_STRATEGY_CONF);
         this.fieldName = this.getString(PARTITION_STRATEGY_FIELD_CONF);
+        this.pathFormat = this.getString(PARTITION_STRATEGY_TIME_PATH_FORMAT_CONF);
+        this.timeBucket = this.getString(PARTITION_STRATEGY_TIME_BUCKET_MS_CONF);
+        this.timezone = this.getString(PARTITION_STRATEGY_TIME_TIMEZONE_CONF);
+        this.timeExtractor = this.getString(PARTITION_STRATEGY_TIME_EXTRACTOR_CONF);
     }
 
 
@@ -75,7 +109,7 @@ public class AzureBlobSinkConfig extends AbstractConfig {
 
     public static void defineConfig(ConfigDef configDef) {
         configDef
-                .define(
+                .define( // MANDATORY
                         CONN_STRING_CONF,
                         ConfigDef.Type.STRING,
                         ConfigDef.NO_DEFAULT_VALUE,
@@ -83,12 +117,12 @@ public class AzureBlobSinkConfig extends AbstractConfig {
                         ConfigDef.Importance.HIGH,
                         CONN_STRING_DOC
                 )
-                .define(
+                .define( // OPTIONAL BUT IMPORTANT
                         CONTAINER_NAME_CONF,
                         ConfigDef.Type.STRING,
                         CONTAINER_NAME_DEFAULT,
                         CONTAINER_NAME_VALIDATOR,
-                        ConfigDef.Importance.LOW,
+                        ConfigDef.Importance.HIGH,
                         CONTAINER_NAME_DOC
                 )
                 .define(
@@ -97,19 +131,18 @@ public class AzureBlobSinkConfig extends AbstractConfig {
                         ConfigDef.Importance.LOW,
                         BLOB_IDENTIFIER_KEY_DOC
                 )
-                .define( // Name of directory to store the records
+                .define( // MANDATORY, Name of directory to store the records
                         TOPIC_DIR,
                         ConfigDef.Type.STRING,
                         ConfigDef.NO_DEFAULT_VALUE,
                         NON_EMPTY_STRING_VALIDATOR,
-                        ConfigDef.Importance.MEDIUM,
+                        ConfigDef.Importance.HIGH,
                         TOPIC_DIR_DOC
                 )
                 .define( // Partition strategy configuration
                         PARTITION_STRATEGY_CONF,
                         ConfigDef.Type.STRING,
                         PARTITION_STRATEGY_DEFAULT,
-                        NON_EMPTY_STRING_VALIDATOR,
                         ConfigDef.Importance.MEDIUM,
                         PARTITION_STRATEGY_DOC
                 )
@@ -119,6 +152,34 @@ public class AzureBlobSinkConfig extends AbstractConfig {
                         PARTITION_STRATEGY_FIELD_DEFAULT,
                         ConfigDef.Importance.MEDIUM,
                         PARTITION_STRATEGY_FIELD_DOC
+                )
+                .define(
+                        PARTITION_STRATEGY_TIME_PATH_FORMAT_CONF,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.NO_DEFAULT_VALUE,
+                        ConfigDef.Importance.LOW,
+                        PARTITION_STRATEGY_TIME_PATH_FORMAT_DOC
+                )
+                .define(
+                        PARTITION_STRATEGY_TIME_TIMEZONE_CONF,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.NO_DEFAULT_VALUE,
+                        ConfigDef.Importance.LOW,
+                        PARTITION_STRATEGY_TIME_TIMEZONE_DOC
+                )
+                .define(
+                        PARTITION_STRATEGY_TIME_EXTRACTOR_CONF,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.NO_DEFAULT_VALUE,
+                        ConfigDef.Importance.LOW,
+                        PARTITION_STRATEGY_TIME_EXTRACTOR_DOC
+                )
+                .define(
+                        PARTITION_STRATEGY_TIME_BUCKET_MS_CONF,
+                        ConfigDef.Type.STRING,
+                        ConfigDef.NO_DEFAULT_VALUE,
+                        ConfigDef.Importance.LOW,
+                        PARTITION_STRATEGY_TIME_BUCKET_MS_DOC
                 );
     }
 
