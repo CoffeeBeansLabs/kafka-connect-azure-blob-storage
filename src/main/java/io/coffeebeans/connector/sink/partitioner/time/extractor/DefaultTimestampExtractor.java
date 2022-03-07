c63ad6a25092fa698632e2103c089a2c5a142418
vishal bihani <vishal@coffeebeans.io>
2022-03-08 00:20:36 +0530

    Added Default Timestamp extractor

---
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/time/extractor/DefaultTimestampExtractor.java
@@ -0,0 +1,47 @@
+package io.coffeebeans.connector.sink.partitioner.time.extractor;
+
+import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+public class DefaultTimestampExtractor implements TimestampExtractor {
+    private static final Logger logger = LoggerFactory.getLogger(DefaultTimestampExtractor.class);
+
+    protected String timezone;
+    protected String pathFormat;
+    protected DateTimeFormatter formatter;
+
+    @Override
+    public void configure(Map<String, String> configProps) {
+        this.timezone = configProps.get(AzureBlobSinkConfig.PARTITION_STRATEGY_TIME_TIMEZONE_CONF);
+        this.pathFormat = configProps.get(AzureBlobSinkConfig.PARTITION_STRATEGY_TIME_PATH_FORMAT_CONF);
+
+        // Initialize the formatter
+        this.formatter = DateTimeFormatter.ofPattern(pathFormat);
+    }
+
+    /**
+     * Get the system date & time and format into the pathFormat and return
+     * @param sinkRecord The sink record
+     * @return Formatted date & time
+     */
+    @Override
+    public String getFormattedTimestamp(SinkRecord sinkRecord) {
+
+        // Get zoned date and time
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of(timezone));
+
+        // Format the zoned date & time
+        String formattedTimestamp = formatter.format(zonedDateTime);
+        logger.debug("Formatted date & time: {}", formattedTimestamp);
+
+        // Return the formatted date and time string
+        return formattedTimestamp;
+    }
+}
