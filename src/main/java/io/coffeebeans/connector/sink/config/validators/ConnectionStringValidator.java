96b4807f2c8b1773e9cc418da4296b180fc85bcc
vishal bihani <vishal@coffeebeans.io>
2022-03-20 16:29:38 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/config/validators/ConnectionStringValidator.java
+++ src/main/java/io/coffeebeans/connector/sink/config/validators/ConnectionStringValidator.java
@@ -4,10 +4,14 @@ import com.azure.storage.blob.BlobServiceClientBuilder;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigException;
 
+/**
+ * This class will validate Connection URL String provided by the user as configuration parameter.
+ */
 public class ConnectionStringValidator implements ConfigDef.Validator {
 
     /**
      * Ensure the provided connection string is not null, empty, blank and malformed.
+     *
      * @param configurationKey Configuration key
      * @param configurationValue Connection String
      */
