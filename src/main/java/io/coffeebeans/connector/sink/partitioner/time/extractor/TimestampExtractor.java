c28aa19711021afeee19e9778c3042479fa07c87
vishal bihani <vishal@coffeebeans.io>
2022-03-20 17:00:43 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/partitioner/time/extractor/TimestampExtractor.java
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/time/extractor/TimestampExtractor.java
@@ -1,19 +1,23 @@
 package io.coffeebeans.connector.sink.partitioner.time.extractor;
 
+import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.util.Map;
-
+/**
+ * To extract timestamp based on the strategy configured.
+ */
 public interface TimestampExtractor {
 
     /**
-     * Configure the timestamp extractor
+     * Configure the timestamp extractor.
+     *
      * @param configProps Map of configurations
      */
     void configure(Map<String, String> configProps);
 
     /**
-     * It returns the formatted date and time
+     * It returns the formatted date and time.
+     *
      * @param sinkRecord SinkRecord
      * @return Formatted date & time string
      */
