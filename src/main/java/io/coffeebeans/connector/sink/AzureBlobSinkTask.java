09884ecf8e669adb5fc72ac43f65d7ebbbd3fdab
vishal bihani <vishal@coffeebeans.io>
2022-03-20 16:26:21 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
+++ src/main/java/io/coffeebeans/connector/sink/AzureBlobSinkTask.java
@@ -4,27 +4,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
 import io.coffeebeans.connector.sink.partitioner.DefaultPartitioner;
-import io.coffeebeans.connector.sink.partitioner.field.FieldPartitioner;
 import io.coffeebeans.connector.sink.partitioner.Partitioner;
+import io.coffeebeans.connector.sink.partitioner.field.FieldPartitioner;
 import io.coffeebeans.connector.sink.partitioner.time.TimeBasedPartitioner;
 import io.coffeebeans.connector.sink.storage.AzureBlobStorageManager;
 import io.coffeebeans.connector.sink.util.StructToMap;
 import io.coffeebeans.connector.sink.util.Version;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
-
+/**
+ * This class will be initialized by the SinkConnector class and will be passed with configuration
+ * props.
+ */
 public class AzureBlobSinkTask extends SinkTask {
     private static final Logger logger = LoggerFactory.getLogger(AzureBlobSinkTask.class);
 
     private long startingOffset;
     private String containerName;
     private Partitioner partitioner;
-//    private String blobIdentifierKey;
     private ObjectMapper objectMapper;
     private AzureBlobStorageManager storageManager;
 
@@ -39,21 +44,21 @@ public class AzureBlobSinkTask extends SinkTask {
         AzureBlobSinkConfig config = new AzureBlobSinkConfig(configProps);
 
         containerName = config.getContainerName();
-//        blobIdentifierKey = config.getBlobIdentifier();
         objectMapper = new ObjectMapper();
         storageManager = new AzureBlobStorageManager(config.getConnectionString());
 
         storageManager.configure(configProps);
         setPartitioner(configProps);
 
-//        logger.debug("Blob identifier key: {}", this.blobIdentifierKey);
         logger.debug("Container name: {}", this.containerName);
         logger.debug("Connection url: {}", config.getConnectionString());
     }
 
     @Override
     public void put(Collection<SinkRecord> collection) {
-        if (collection.isEmpty()) return;
+        if (collection.isEmpty()) {
+            return;
+        }
 
         List<SinkRecord> records = new ArrayList<>(collection);
         logger.debug("Received {} records", records.size());
@@ -72,15 +77,19 @@ public class AzureBlobSinkTask extends SinkTask {
             if (record.value() instanceof Map) {
                 valueMap = (Map<?, ?>) record.value();
 
-            } else if (record.value() instanceof Struct){
+            } else if (record.value() instanceof Struct) {
                 // Convert Struct to Map
                 valueMap = StructToMap.toJsonMap((Struct) record.value());
 
-            } else return;
+            } else {
+                return;
+            }
 
 
             // Check if record is empty; if empty then return;
-            if (valueMap.isEmpty()) return;
+            if (valueMap.isEmpty()) {
+                return;
+            }
 
             // Get the blob name using the identifier key
             // String blobName = (String) valueMap.get(blobIdentifierKey);
@@ -111,18 +120,20 @@ public class AzureBlobSinkTask extends SinkTask {
 
     private void setPartitioner(Map<String, String> configProps) {
         switch (configProps.get(AzureBlobSinkConfig.PARTITION_STRATEGY_CONF)) {
-            case "FIELD": partitioner = new FieldPartitioner();
+          case "FIELD": partitioner = new FieldPartitioner();
             break;
-            case "TIME": partitioner = new TimeBasedPartitioner();
+          case "TIME": partitioner = new TimeBasedPartitioner();
             break;
 
-            default: partitioner = new DefaultPartitioner();
+          default: partitioner = new DefaultPartitioner();
         }
         partitioner.configure(configProps);
     }
 
     private void setStartingOffset(long offset) {
-        if (startingOffset == 0) startingOffset = offset;
+        if (startingOffset == 0) {
+            startingOffset = offset;
+        }
     }
 
 }
