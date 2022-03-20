fdce8a71eda795085c401dcae62357680c71e412
vishal bihani <vishal@coffeebeans.io>
2022-03-20 16:34:54 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/partitioner/time/DailyPartitioner.java
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/time/DailyPartitioner.java
@@ -1,10 +1,12 @@
 package io.coffeebeans.connector.sink.partitioner.time;
 
 import io.coffeebeans.connector.sink.partitioner.Partitioner;
+import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.util.Map;
-
+/**
+ * Partitioner to partition incoming records based on day.
+ */
 public class DailyPartitioner implements Partitioner {
     private static final String PATH_FORMAT = "year={0}/month={1}/day={2}";
 
@@ -14,13 +16,17 @@ public class DailyPartitioner implements Partitioner {
     }
 
     /**
-     * Return file path based on today's date
+     * Return file path based on today's date.
+     *
      * @param sinkRecord The record to be stored
      * @param startingOffset kafka offset of the first record of the batch
-     * @return <prefix>/<topic>/year='YYYY'/month='MM'/day='dd'/<topic>+<kafkaPartition>+<startOffset>.<format>
+     * @return encoded partition string
      */
     @Override
     public String encodePartition(SinkRecord sinkRecord, long startingOffset) {
+
+        // Output format
+        // <prefix>/<topic>/year='YYYY'/month='MM'/day='dd'/<topic>+<kafkaPartition>+<startOffset>.<format>
         return null;
     }
 }
