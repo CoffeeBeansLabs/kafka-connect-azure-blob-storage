b888749b13a64b3107374b8a6b9637362bc6520c
vishal bihani <vishal@coffeebeans.io>
2022-03-20 17:23:35 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/config/recommenders/PartitionStrategyRecommender.java
+++ src/main/java/io/coffeebeans/connector/sink/config/recommenders/PartitionStrategyRecommender.java
@@ -1,12 +1,14 @@
 package io.coffeebeans.connector.sink.config.recommenders;
 
 import io.coffeebeans.connector.sink.partitioner.PartitionStrategy;
-import org.apache.kafka.common.config.ConfigDef;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.kafka.common.config.ConfigDef;
 
+/**
+ * This recommender will recommend valid values for the partition strategy configuration.
+ */
 public class PartitionStrategyRecommender implements ConfigDef.Recommender {
 
     @Override
