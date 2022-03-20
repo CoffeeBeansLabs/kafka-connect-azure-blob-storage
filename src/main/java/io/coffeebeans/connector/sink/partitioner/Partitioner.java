ba890522534c5e8001fd18e4dadcb1219a8a88bf
vishal bihani <vishal@coffeebeans.io>
2022-03-20 17:22:22 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/partitioner/Partitioner.java
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/Partitioner.java
@@ -1,19 +1,23 @@
 package io.coffeebeans.connector.sink.partitioner;
 
+import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.util.Map;
-
+/**
+ * Partitioner to partition the incoming fields based on the strategy configured.
+ */
 public interface Partitioner {
 
     /**
-     * Configure the partitioner
+     * Configure the partitioner.
+     *
      * @param configProps configuration parameters like partition strategy, etc.
      */
     void configure(Map<String, String> configProps);
 
     /**
      * Returns the string representing path where it will be stored.
+     *
      * @param sinkRecord The record to be stored
      * @return The path where it will be stored
      */
