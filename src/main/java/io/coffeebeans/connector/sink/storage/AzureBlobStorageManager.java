6c0e5bae7e394666a3520b18f0e1717638fb1ec6
vishal bihani <vishal@coffeebeans.io>
2022-03-20 17:29:47 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/storage/AzureBlobStorageManager.java
+++ src/main/java/io/coffeebeans/connector/sink/storage/AzureBlobStorageManager.java
@@ -5,26 +5,34 @@ import com.azure.core.util.Context;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-import com.azure.storage.blob.models.*;
+import com.azure.storage.blob.models.AppendBlobItem;
+import com.azure.storage.blob.models.AppendBlobRequestConditions;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import com.azure.storage.blob.models.BlobStorageException;
 import com.azure.storage.blob.options.AppendBlobCreateOptions;
 import com.azure.storage.blob.specialized.AppendBlobClient;
-import com.azure.storage.blob.specialized.BlobClientBase;
 import com.azure.storage.blob.specialized.BlobLeaseClient;
 import com.azure.storage.blob.specialized.BlobLeaseClientBuilder;
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import java.io.ByteArrayInputStream;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.ByteArrayInputStream;
-import java.time.Duration;
-import java.util.Map;
-
+/**
+ * This class will handle the implementation for uploading the data to blob storage service.
+ */
 public class AzureBlobStorageManager {
     private static final Logger logger = LoggerFactory.getLogger(AzureBlobStorageManager.class);
     private final BlobServiceClient serviceClient;
 
     private Long maxBlobSize;
 
+    /**
+     * Constructor to be used for initializing the storage manager.
+     *
+     * @param connectionUrl Connection url provided by the user in the configuration.
+     */
     public AzureBlobStorageManager(String connectionUrl) {
         // Init service client
         this.serviceClient = new BlobServiceClientBuilder()
@@ -36,6 +44,14 @@ public class AzureBlobStorageManager {
         this.maxBlobSize = Long.parseLong(configProps.get(AzureBlobSinkConfig.ROLLOVER_POLICY_SIZE_CONF));
     }
 
+    /**
+     * I need the container name, blob name and the byte array of the data. I will upload the data to the container and
+     * blob provided.
+     *
+     * @param containerName Container name
+     * @param blobName Blob name
+     * @param data Data as byte stream
+     */
     public void upload(String containerName, String blobName, byte[] data) {
         BlobContainerClient containerClient = this.serviceClient
                 .getBlobContainerClient(containerName);
@@ -44,7 +60,7 @@ public class AzureBlobStorageManager {
         createAppendBlobIfNotExist(appendBlobClient);
 
         try {
-//            appendBlobClient.appendBlock(new ByteArrayInputStream(data), data.length);
+            // appendBlobClient.appendBlock(new ByteArrayInputStream(data), data.length);
             Response<AppendBlobItem> response = appendBlobClient.appendBlockWithResponse(
                     new ByteArrayInputStream(data), data.length, null,
                     new AppendBlobRequestConditions().setMaxSize(maxBlobSize),
@@ -65,10 +81,10 @@ public class AzureBlobStorageManager {
                 .getBlobClient(blobName + "." + (sourceFileNumber + 1)).getAppendBlobClient();
 
         if (destAppendBlobClient.exists()) {
-            rollover(blobContainerClient, blobName, sourceFileNumber+1);
+            rollover(blobContainerClient, blobName, sourceFileNumber + 1);
         }
 
-        moveToNewBlob(blobContainerClient, blobName, sourceFileNumber, sourceFileNumber+1);
+        moveToNewBlob(blobContainerClient, blobName, sourceFileNumber, sourceFileNumber + 1);
     }
 
     private void moveToNewBlob(BlobContainerClient blobContainerClient, String blobName, int sourceFileNumber,
