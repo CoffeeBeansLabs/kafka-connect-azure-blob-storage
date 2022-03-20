df30e5a703961364cba2848c95b0ec16dceff653
vishal bihani <vishal@coffeebeans.io>
2022-03-20 16:40:40 +0530

    Refactored for checkstyle violations

--- src/main/java/io/coffeebeans/connector/sink/partitioner/DefaultPartitioner.java
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/DefaultPartitioner.java
@@ -1,10 +1,12 @@
 package io.coffeebeans.connector.sink.partitioner;
 
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 
-import java.util.Map;
-
+/**
+ * The DefaultPartitioner partitions the incoming data based on the prefix, kafka topic, and kafka partition.
+ */
 public class DefaultPartitioner implements Partitioner {
     public static final String FOLDER_DELIMITER = "/";
     public static final String FILE_DELIMITER = "+";
@@ -19,23 +21,29 @@ public class DefaultPartitioner implements Partitioner {
     }
 
     /**
+     * I need the SinkRecord and the starting offset. I will create an encoded partition based on prefix, kafka topic,
+     * kafka partition and starting offset.
+     *
      * @param sinkRecord The record to be stored
-     * @return <prefix>/<kafkaTopic>/partition=<kafkaPartition>/<kafkaTopic>+<kafkaPartition>+<startOffset>.<format>
+     * @return encoded partition string
      */
     @Override
     public String encodePartition(SinkRecord sinkRecord, long startingOffset) {
         String kafkaTopic = sinkRecord.topic();
         long kafkaPartition = sinkRecord.kafkaPartition();
 
-        return prefix + FOLDER_DELIMITER + // <prefix>/
+        // Output format:
+        // <prefix>/<kafkaTopic>/partition=<kafkaPartition>/<kafkaTopic>+<kafkaPartition>+<startOffset>.<format>
+
+        return prefix + FOLDER_DELIMITER // <prefix>/
 
                 // <kafkaTopic>/
-                kafkaTopic + FOLDER_DELIMITER +
+                + kafkaTopic + FOLDER_DELIMITER
 
                 // partition=<kafkaPartition>/
-                kafkaPartitionProperty + "=" + kafkaPartition + FOLDER_DELIMITER +
+                + kafkaPartitionProperty + "=" + kafkaPartition + FOLDER_DELIMITER
 
                 // <kafkaTopic> + <kafkaPartition> + <startOffset>
-                kafkaTopic + FILE_DELIMITER + kafkaPartition + FILE_DELIMITER + startingOffset;
+                + kafkaTopic + FILE_DELIMITER + kafkaPartition + FILE_DELIMITER + startingOffset;
     }
 }
