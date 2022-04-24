a8e47eba104d0b11d93d5e91c72de0c7adf8676d
vishal bihani <vishal@coffeebeans.io>
2022-04-25 00:09:54 +0530

    [KABSC-46]: Refactored

--- src/main/java/io/coffeebeans/connector/sink/partitioner/field/FieldPartitioner.java
+++ src/main/java/io/coffeebeans/connector/sink/partitioner/field/FieldPartitioner.java
@@ -7,26 +7,28 @@ import io.coffeebeans.connector.sink.partitioner.PartitionerUtil;
 import org.apache.kafka.connect.sink.SinkRecord;
 
 /**
- * This partitioner will partition the incoming records based on the value of the field specified by the user.
+ * This partitioner will partition the incoming records based on the value of the field specified.
  */
 public class FieldPartitioner extends DefaultPartitioner {
     private final String fieldName;
 
     /**
-     * Constructor with Config class object as parameter.
+     * Constructor.
      *
      * @param config AzureBlobSinkConfig config class object
      */
     public FieldPartitioner(AzureBlobSinkConfig config) {
         super(config);
 
-        this.fieldName = config.getFieldName();
-        logger.info("Field name configured: {}", fieldName);
+        fieldName = config.getFieldName();
+        log.info("Field name configured: {}", fieldName);
     }
 
     /**
-     * I need SinkRecord and starting offset as the parameters. I will extract the value from the field specified by the
-     * user and will generate the encoded partition string using that.
+     * Generate the encoded partition string by extracting the value of the
+     * specified field.
+     *
+     * <p>&lt;fieldName&gt;=&lt;fieldValue&gt;
      *
      * @param sinkRecord The sink record to be stored
      * @return Encoded partition string
@@ -35,10 +37,6 @@ public class FieldPartitioner extends DefaultPartitioner {
     public String encodePartition(SinkRecord sinkRecord) throws JsonProcessingException {
         String fieldValue = PartitionerUtil.getFieldValueAsString(sinkRecord, fieldName);
 
-        /*
-          Output format:
-          <fieldName>=<fieldValue>
-         */
         return fieldName + "=" + fieldValue;
     }
 }
