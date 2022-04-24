daef096273635c87d17695e33c02adcbbe48637e
vishal bihani <vishal@coffeebeans.io>
2022-04-25 01:26:18 +0530

    [KABSC-46]: Refactored

--- src/main/java/io/coffeebeans/connector/sink/TopicPartitionWriter.java
+++ src/main/java/io/coffeebeans/connector/sink/TopicPartitionWriter.java
@@ -2,20 +2,28 @@ package io.coffeebeans.connector.sink;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
+import io.coffeebeans.connector.sink.format.RecordWriter;
+import io.coffeebeans.connector.sink.format.RecordWriterProvider;
 import io.coffeebeans.connector.sink.partitioner.Partitioner;
-import io.coffeebeans.connector.sink.storage.RecordWriter;
-import io.coffeebeans.connector.sink.storage.RecordWriterProvider;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.sink.ErrantRecordReporter;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.*;
-
+/**
+ * TopicPartitionWriter will be unique for each topic-partition. It will be
+ * responsible for batching the records, performing rotation, and maintaining offsets.
+ * It will also maintain a map of writers unique to each encoded partition.
+ */
 public class TopicPartitionWriter {
-    private static final Logger logger = LoggerFactory.getLogger(TopicPartitionWriter.class);
+    private static final Logger log = LoggerFactory.getLogger(TopicPartitionWriter.class);
 
     private final int flushSize;
     private final long dataSize;
@@ -23,91 +31,133 @@ public class TopicPartitionWriter {
     private final Partitioner partitioner;
     private final Queue<SinkRecord> buffer;
     private final AzureBlobSinkConfig config;
-    private final TopicPartition topicPartition;
     private final ErrantRecordReporter reporter;
     private final RecordWriterProvider recordWriterProvider;
 
     private final Map<String, Long> startTimes;
-    private final Map<String, Long> startOffsets;
     private final Map<String, Long> recordsCount;
     private final Map<String, RecordWriter> writers;
 
+    /**
+     * Constructor.
+     *
+     * @param topicPartition Kafka topic partition
+     * @param config Connector config object
+     * @param reporter DLQ Error reporter
+     * @param partitioner Partitioner
+     * @param recordWriterProvider Record writer provider
+     */
     public TopicPartitionWriter(TopicPartition topicPartition,
                                 AzureBlobSinkConfig config,
                                 ErrantRecordReporter reporter,
                                 Partitioner partitioner,
                                 RecordWriterProvider recordWriterProvider) {
 
-        this.topicPartition = topicPartition;
-        this.reporter = reporter;
         this.config = config;
+        this.reporter = reporter;
         this.partitioner = partitioner;
         this.buffer = new LinkedList<>();
+        this.dataSize = config.getFileSize();
+        this.flushSize = config.getFlushSize();
+        this.recordWriterProvider = recordWriterProvider;
+        this.rotationIntervalMs = config.getRotationIntervalMs();
 
         this.writers = new HashMap<>();
         this.startTimes = new HashMap<>();
-        this.dataSize = config.getFileSize();
-        this.flushSize = config.getFlushSize();
-        this.rotationIntervalMs = config.getRotationIntervalMs();
-        this.startOffsets = new HashMap<>();
         this.recordsCount = new HashMap<>();
-        this.recordWriterProvider = recordWriterProvider;
     }
 
+    /**
+     * Buffer. Add record to the buffer queue.
+     *
+     * @param sinkRecord Record to be put into buffer
+     */
     public void buffer(SinkRecord sinkRecord) {
         buffer.add(sinkRecord);
     }
 
+    /**
+     * It polls records from buffer and write it using RecordWriter.
+     * It also checks for rotation before and after record is written.
+     *
+     * @throws JsonProcessingException If any processing exception occurs
+     */
     public void write() throws JsonProcessingException {
         long now = System.currentTimeMillis();
 
         while (!buffer.isEmpty()) {
-             // Get the Record writer using encoded partition and write the record to the writer.
             SinkRecord record = buffer.poll();
             String encodedPartition = partitioner.encodePartition(record);
-            rotateIfFlushSizeConditionMet(encodedPartition);
-            rotateIfDataSizeConditionMet(encodedPartition);
+            rotateIfFlushOrDataSizeConditionMet(encodedPartition);
 
             RecordWriter writer = writers.get(encodedPartition);
 
             if (writer == null) {
                 // Writer does not exist so create a new one
                 writer = instantiateNewWriter(record, encodedPartition);
-                startTimes.put(encodedPartition, now);
             }
 
             try {
                 writer.write(record);
+                startTimes.put(encodedPartition, now);
                 recordsCount.put(encodedPartition, recordsCount.get(encodedPartition) + 1);
 
             } catch (Exception e) {
-                logger.error("Failed to write record with offset: {}, encodedPartition: {}, sending to DLQ",
+                log.error("Failed to write record with offset: {}, encodedPartition: {}, sending to DLQ",
                         record.kafkaOffset(), encodedPartition);
-                logger.error(e.getMessage());
-                e.printStackTrace();
-                //                reporter.report(record, e);
+                reporter.report(record, e);
             }
         }
-        checkForRotationAndPerform(now);
+        rotateIfRotateIntervalMsConditionMet(now);
     }
 
+    /**
+     * Instantiate a new RecordWriter.
+     *
+     * @param record Record to be processed
+     * @param encodedPartition encoded partition
+     * @return Record writer to write the record
+     */
     private RecordWriter instantiateNewWriter(SinkRecord record, String encodedPartition) {
         RecordWriter writer = recordWriterProvider.getRecordWriter(
-                config, partitioner.generateFullPath(record, encodedPartition, record.kafkaOffset()));
+                config, partitioner.generateFullPath(record, encodedPartition, record.kafkaOffset())
+        );
 
         writers.put(encodedPartition, writer);
-        startOffsets.put(encodedPartition, record.kafkaOffset());
         recordsCount.put(encodedPartition, 0L);
         return writer;
     }
 
-    private void checkForRotationAndPerform(long currentTime) {
+    /**
+     * If the flush size or data size condition is met then rotation will be done.
+     *
+     * @param encodedPartition encoded partition
+     */
+    private void rotateIfFlushOrDataSizeConditionMet(String encodedPartition) {
+        if (isFlushSizeConditionMet(encodedPartition) || isDataSizeConditionMet(encodedPartition)) {
+            commit(encodedPartition);
+        }
+    }
+
+    /**
+     * Iterate through the record writers and their start times. If the
+     * difference between the start time and current time is more than
+     * rotate interval then perform rotation.
+     *
+     * @param currentTime current server time
+     */
+    private void rotateIfRotateIntervalMsConditionMet(long currentTime) {
+        if (rotationIntervalMs < 0) {
+            // Condition to check if rotation based on time is enabled or not.
+            return;
+        }
+
         Iterator<Map.Entry<String, RecordWriter>> iterator = writers.entrySet().iterator();
 
         while (iterator.hasNext()) {
             Map.Entry<String, RecordWriter> entry = iterator.next();
             String encodedPartition = entry.getKey();
-            if (!shouldRotate(encodedPartition, currentTime)) {
+            if (!isRotateIntervalMsConditionMet(encodedPartition, currentTime)) {
                 continue;
             }
 
@@ -116,52 +166,61 @@ public class TopicPartitionWriter {
                 entry.getValue().commit();
 
             } catch (IOException e) {
-                logger.error("Failed to commit file with encodedPartition: {}, Removing the writer", encodedPartition);
+                log.error("Failed to commit file with encodedPartition: {}, Removing the writer", encodedPartition);
             }
 
             iterator.remove();
-            startOffsets.remove(encodedPartition);
             recordsCount.remove(encodedPartition);
             startTimes.remove(encodedPartition);
         }
     }
 
-    private void rotateIfFlushSizeConditionMet(String encodedPartition) {
-        if (!isFlushSizeConditionMet(encodedPartition)) {
-            return;
-        }
-        commit(encodedPartition);
-    }
-
-    private void rotateIfDataSizeConditionMet(String encodedPartition) {
-        if (!isDataSizeConditionMet(encodedPartition)) {
-            return;
-        }
-        commit(encodedPartition);
-    }
-
+    /**
+     * Invoked while performing rotation (rolling file).
+     * It commits the RecordWriter and removes the RecordWriter
+     * from all the mappings.
+     *
+     * @param encodedPartition encoded partition
+     */
     private void commit(String encodedPartition) {
         RecordWriter writer = writers.get(encodedPartition);
         if (writer == null) {
-            logger.warn("Writer not available to commit. Ignoring");
+            log.warn("Writer not available to commit. Ignoring");
             return;
         }
         try {
             writer.commit();
 
         } catch (IOException e) {
-            logger.error("Failed to commit file with encodedPartition: {}, Removing the writer", encodedPartition);
+            log.error("Failed to commit file with encodedPartition: {}, Removing the writer", encodedPartition);
         }
         writers.remove(encodedPartition);
-        startOffsets.remove(encodedPartition);
         startTimes.remove(encodedPartition);
         recordsCount.remove(encodedPartition);
     }
 
+    /**
+     * If the data size condition is met or not. Data size is the
+     * amount of data a RecordWriter should write after
+     * which the rotation should happen.
+     *
+     * <p>Useful in scenarios when the sink has a max limit on file size.
+     *
+     * @param encodedPartition encoded partition
+     * @return Whether the data size condition is met or not
+     */
     private boolean isDataSizeConditionMet(String encodedPartition) {
         return writers.get(encodedPartition) != null && writers.get(encodedPartition).getDataSize() >= dataSize;
     }
 
+    /**
+     * If the flush condition is met or not. Flush size is the
+     * number of records a RecordWriter should process after
+     * which the rotation should happen.
+     *
+     * @param encodedPartition encoded partition
+     * @return Whether the flush size condition is met or not
+     */
     private boolean isFlushSizeConditionMet(String encodedPartition) {
         if (flushSize < 0) {
             return false;
@@ -171,26 +230,31 @@ public class TopicPartitionWriter {
         return recordsCount.get(encodedPartition) != null && recordsCount.get(encodedPartition) >= flushSize;
     }
 
-    private boolean shouldRotate(String encodedPartition, long currentTime) {
-        if (isFlushSizeConditionMet(encodedPartition) || isDataSizeConditionMet(encodedPartition)) {
-            return true;
-        }
-
-        // If difference of current and start time equals or exceed rotate interval ms then return true
-        if (rotationIntervalMs < 0) {
-            // Condition to check if rotation based on time is enabled or not.
-            return false;
-        }
+    /**
+     * If rotate interval ms condition is met or not.
+     * Rotate interval ms is the time up to which the RecordWriter
+     * is kept open. After that time is passed the writer will be
+     * committed.
+     *
+     * @param encodedPartition encoded partition
+     * @param currentTime current server time
+     * @return Whether the condition is met or not
+     */
+    private boolean isRotateIntervalMsConditionMet(String encodedPartition, long currentTime) {
         return startTimes.get(encodedPartition) != null
                 && currentTime - startTimes.get(encodedPartition) >= rotationIntervalMs;
     }
 
+    /**
+     * Invoked to close all the RecordWriters and clear mappings.
+     *
+     * @throws IOException If any I/O exception occur
+     */
     public void close() throws IOException {
         for (RecordWriter writer : writers.values()) {
-            writer.close();
+            writer.commit();
         }
         writers.clear();
-        startOffsets.clear();
         startTimes.clear();
         recordsCount.clear();
     }
