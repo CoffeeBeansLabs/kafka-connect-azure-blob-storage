4cd2e6a4db97ba02297709901284169b8c529c40
vishal bihani <vishal@coffeebeans.io>
2022-07-20 14:11:32 +0530

    KABSC-54: Added topic property to the method

--- src/main/java/io/coffeebeans/connector/sink/format/RecordWriterProvider.java
+++ src/main/java/io/coffeebeans/connector/sink/format/RecordWriterProvider.java
@@ -8,5 +8,5 @@ import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
  */
 public interface RecordWriterProvider {
 
-    RecordWriter getRecordWriter(AzureBlobSinkConfig config, String fileName);
+    RecordWriter getRecordWriter(AzureBlobSinkConfig config, String fileName, String topic);
 }
