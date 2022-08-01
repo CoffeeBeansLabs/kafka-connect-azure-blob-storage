1a1ba580a8bba0f7449e8f560af3f98f47290492
vishal bihani <vishal@coffeebeans.io>
2022-08-01 11:27:03 +0530

    [KABSC-57]: Refactored to comply with checkstyle

--- src/main/java/io/coffeebeans/connector/sink/config/recommenders/FileFormatRecommender.java
+++ src/main/java/io/coffeebeans/connector/sink/config/recommenders/FileFormatRecommender.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.kafka.common.config.ConfigDef;
 
 /**
@@ -12,14 +15,30 @@ import org.apache.kafka.common.config.ConfigDef;
  */
 public class FileFormatRecommender implements ConfigDef.Recommender {
 
+    /**
+     * Returns a list of supported file formats by the connector.
+     *
+     * @param fileFormatConf file format conf key
+     * @param configProps Map of user passed configurations
+     * @return List of supported file formats
+     */
     @Override
-    public List<Object> validValues(String fileFormatConfKey, Map<String, Object> parsedConfig) {
-        List<Object> validValues = new ArrayList<>();
-        Arrays.stream(FileFormat.values()).forEach((format) -> validValues.add(format.toString()));
+    public List<Object> validValues(String fileFormatConf, Map<String, Object> configProps) {
 
-        return validValues;
+        FileFormat[] supportedFileFormats = FileFormat.values();
+
+        return Stream.of(supportedFileFormats)
+                .map(FileFormat::name)
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Is this configuration recommended.
+     *
+     * @param s Conf key
+     * @param map Map of user passed configurations
+     * @return True if recommended else false
+     */
     @Override
     public boolean visible(String s, Map<String, Object> map) {
         return true;
