930cd6306733b7e3d4b7a197cf9e7687f9600d3a
vishal bihani <vishal@coffeebeans.io>
2022-08-01 11:36:36 +0530

    [KABSC-57]: Refactored to comply with checkstyle

--- src/main/java/io/coffeebeans/connector/sink/config/recommenders/partitioner/time/TimestampExtractorRecommender.java
+++ src/main/java/io/coffeebeans/connector/sink/config/recommenders/partitioner/time/TimestampExtractorRecommender.java
@@ -3,10 +3,10 @@ package io.coffeebeans.connector.sink.config.recommenders.partitioner.time;
 import io.coffeebeans.connector.sink.config.AzureBlobSinkConfig;
 import io.coffeebeans.connector.sink.partitioner.PartitionStrategy;
 import io.coffeebeans.connector.sink.partitioner.time.extractor.TimestampExtractorStrategy;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.apache.kafka.common.config.ConfigDef;
 
 /**
@@ -14,18 +14,36 @@ import org.apache.kafka.common.config.ConfigDef;
  */
 public class TimestampExtractorRecommender implements ConfigDef.Recommender {
 
+    /**
+     * Returns supported timestamp extractor strategies.
+     *
+     * @param timestampExtractorConf TimestampExtractor configuration key
+     * @param configProps Map of user defined configuration
+     * @return List of supported timestamp strategies
+     */
     @Override
-    public List<Object> validValues(String timestampExtractorConfKey, Map<String, Object> parsedConfig) {
-        List<Object> validValues = new ArrayList<>();
-        Arrays.stream(TimestampExtractorStrategy.values()).forEach((value) -> validValues.add(value.toString()));
+    public List<Object> validValues(String timestampExtractorConf, Map<String, Object> configProps) {
 
-        return validValues;
+        TimestampExtractorStrategy[] supportedExtractorStrategies = TimestampExtractorStrategy.values();
+
+        return Stream.of(supportedExtractorStrategies)
+                .map(TimestampExtractorStrategy::name)
+                .collect(Collectors.toList());
     }
 
+    /**
+     * Returns whether this configuration is recommended or not.
+     * Timestamp extractor strategy configuration is only valid if partition strategy is
+     * configured to TIME.
+     *
+     * @param timestampExtractorConf TimestampExtractor configuration key
+     * @param configProps Map of user defined configuration
+     * @return True if recommended or else false
+     */
     @Override
-    public boolean visible(String timestampExtractorConfKey, Map<String, Object> parsedConfig) {
-        String partitionStrategy = (String) parsedConfig.get(AzureBlobSinkConfig.PARTITION_STRATEGY_CONF_KEY);
+    public boolean visible(String timestampExtractorConf, Map<String, Object> configProps) {
+        String partitionStrategy = (String) configProps.get(AzureBlobSinkConfig.PARTITION_STRATEGY_CONF_KEY);
 
-        return PartitionStrategy.TIME.toString().equals(partitionStrategy);
+        return PartitionStrategy.TIME.toString().equalsIgnoreCase(partitionStrategy);
     }
 }
