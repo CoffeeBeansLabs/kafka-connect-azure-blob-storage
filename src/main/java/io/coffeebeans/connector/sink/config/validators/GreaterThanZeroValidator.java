bff84915f4f768f9c6d1434ed473dd58617ac4ba
Vishal Bihani <vishal.bihani2@gmail.com>
2022-08-11 11:05:12 +0530

    [KABSC-85]: Added GreaterThanZeroValidator

---
+++ src/main/java/io/coffeebeans/connector/sink/config/validators/GreaterThanZeroValidator.java
@@ -0,0 +1,35 @@
+package io.coffeebeans.connector.sink.config.validators;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.config.ConfigException;
+
+/**
+ * {@link ConfigDef.Validator} for configurations which require value to be <br>
+ * more than zero.
+ */
+public class GreaterThanZeroValidator implements ConfigDef.Validator {
+
+    /**
+     * Perform single configuration validation.
+     *
+     * @param name  The name of the configuration
+     * @param value The value of the configuration
+     */
+    @Override
+    public void ensureValid(String name, Object value) {
+
+        if (value instanceof Integer && (int) value > 0) {
+            return;
+
+        } else if (value instanceof Long && (long) value > 0) {
+            return;
+
+        } else if (value instanceof Double && (double) value > 0) {
+            return;
+
+        } else if (value instanceof Float && (float) value > 0) {
+            return;
+        }
+        throw new ConfigException("Cannot be less than 1");
+    }
+}
