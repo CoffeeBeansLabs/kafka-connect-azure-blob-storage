c20c1a2ac778fedc15e08566864d1247e1f269ce
Vishal Bihani <vishal.bihani2@gmail.com>
2022-08-11 06:18:19 +0530

    [KABSC-84]: Updated ParquetOutputStreamTest unit tests

--- src/test/java/io/coffeebeans/connector/sink/format/parquet/ParquetOutputStreamTest.java
+++ src/test/java/io/coffeebeans/connector/sink/format/parquet/ParquetOutputStreamTest.java
@@ -1,27 +1,49 @@
 package io.coffeebeans.connector.sink.format.parquet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.anyBoolean;
+import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 import io.coffeebeans.connector.sink.storage.StorageManager;
 import java.io.IOException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
 
 /**
- * Unit tests for ParquetOutputStream.
+ * Unit tests for {@link ParquetOutputStream}.
  */
+@ExtendWith(MockitoExtension.class)
 public class ParquetOutputStreamTest {
 
+    @Mock
     private StorageManager storageManager;
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given byte</li>
+     *     <li>Buffer is not full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>Should not invoke {@link StorageManager}</li>
+     * </ul>
+     */
     @Test
-    @DisplayName("Given byte, when buffer size is not full write method should not invoke append method")
-    void write_givenByte_whenBufferIsNotFull_shouldNotInvokeAppendMethod() throws IOException {
+    @DisplayName("Given byte, when buffer size is not full write method should not invoke storage manager")
+    void write_givenByte_whenBufferIsNotFull_shouldNotInvokeStorageManager() throws IOException {
 
         String blobName = "test-blob";
         int partSize = 10;
@@ -31,7 +53,6 @@ public class ParquetOutputStreamTest {
         writes the byte into the buffer, there is still some space available.
          */
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         int inputByte = 5;
@@ -42,12 +63,25 @@ public class ParquetOutputStreamTest {
         verifyNoInteractions(storageManager);
 
         // Position will be incremented by 1 position after writing.
-        Assertions.assertEquals(1, outputStream.getPos());
+        assertEquals(1, outputStream.getPos());
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given byte</li>
+     *     <li>Buffer is full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>Should invoke {@link StorageManager}</li>
+     * </ul>
+     */
     @Test
-    @DisplayName("Given byte, when buffer size is full write method should invoke append method exactly once")
-    void write_givenByte_whenBufferIsFull_shouldInvokeAppendMethodExactlyOnce() throws IOException {
+    @DisplayName("Given byte, when buffer size is full write method should invoke storage manager")
+    void write_givenByte_whenBufferIsFull_shouldInvokeStorageManager() throws IOException {
 
         String blobName = "test-blob";
         int partSize = 1;
@@ -58,17 +92,35 @@ public class ParquetOutputStreamTest {
         it using the upload method thus invoking the append method of the storage manager.
          */
 
-        storageManager = mock(StorageManager.class);
+        when(storageManager.stageBlockAsync(anyString(), anyString(), any()))
+                .thenReturn(Mono.empty());
+
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         int inputByte = 5;
 
         outputStream.write(inputByte);
 
-        verify(storageManager, times(1));
-        Assertions.assertEquals(1, outputStream.getPos());
+        verify(storageManager, times(1))
+                .stageBlockAsync(anyString(), anyString(), any());
+
+        assertEquals(1, outputStream.getPos());
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given null byte array</li>
+     *     <li>Buffer is not full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>{@link ParquetOutputStream#write(byte[], int, int)} should throw<br>
+     *     {@link NullPointerException}</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given null byte array, write should throw NullPointerException")
     void write_givenNullByteArray_shouldThrowNullPointerException() {
@@ -78,13 +130,26 @@ public class ParquetOutputStreamTest {
         int offset = 3;
         int length = 6;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
-        Assertions.assertThrowsExactly(NullPointerException.class,
+        assertThrowsExactly(NullPointerException.class,
                 () -> outputStream.write(null, offset, length));
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given offset > length</li>
+     *     <li>Buffer is not full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>{@link ParquetOutputStream#write(byte[], int, int)} should <br>
+     *     throw {@link IndexOutOfBoundsException}</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given offset more than byte array length, write should throw index out of bounds exception")
     void write_givenOffsetMoreThanByteArrayLength_shouldThrowIndexOutOfBoundsException() {
@@ -92,7 +157,6 @@ public class ParquetOutputStreamTest {
         String blobName = "test-blob";
         int partSize = 1;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         /*
@@ -103,10 +167,24 @@ public class ParquetOutputStreamTest {
         int offset = 15;
         int length = 5;
 
-        Assertions.assertThrowsExactly(IndexOutOfBoundsException.class,
+        assertThrowsExactly(IndexOutOfBoundsException.class,
                 () -> outputStream.write(bytesToBeWritten, offset, length));
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given length < 0</li>
+     *     <li>Buffer is not full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>{@link ParquetOutputStream#write(byte[], int, int)} should <br>
+     *     throw {@link IndexOutOfBoundsException}</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given length less than zero, write should throw index out of bounds exception")
     void write_givenLengthLessThanZero_shouldThrowIndexOutOfBoundsException() {
@@ -114,7 +192,6 @@ public class ParquetOutputStreamTest {
         String blobName = "test-blob";
         int partSize = 1;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         /*
@@ -125,10 +202,24 @@ public class ParquetOutputStreamTest {
         int offset = 5;
         int length = -1;
 
-        Assertions.assertThrowsExactly(IndexOutOfBoundsException.class,
+        assertThrowsExactly(IndexOutOfBoundsException.class,
                 () -> outputStream.write(bytesToBeWritten, offset, length));
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given length + offset > array length</li>
+     *     <li>Buffer is not full</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>{@link ParquetOutputStream#write(byte[], int, int)} should <br>
+     *     throw {@link IndexOutOfBoundsException}</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given sum of length and offset more than byte array length, write should "
             + "throw index out of bounds exception")
@@ -137,7 +228,6 @@ public class ParquetOutputStreamTest {
         String blobName = "test-blob";
         int partSize = 1;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         /*
@@ -148,10 +238,22 @@ public class ParquetOutputStreamTest {
         int offset = 6;
         int length = 5;
 
-        Assertions.assertThrowsExactly(IndexOutOfBoundsException.class,
+        assertThrowsExactly(IndexOutOfBoundsException.class,
                 () -> outputStream.write(bytesToBeWritten, offset, length));
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Given byte 3 times more than buffer size</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>Should invoke {@link StorageManager} 3 times</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given bytes three times more than buffer size, should invoke storage manager three times")
     void write_givenBytesThreeTimesMoreThanBufferSize_shouldInvokeStorageManagerThreeTimes() throws IOException {
@@ -159,21 +261,37 @@ public class ParquetOutputStreamTest {
         String blobName = "test-blob";
         int partSize = 3;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         byte[] bytesToBeWritten = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int offset = 0;
         int length = 9;
 
+        // Mocking staging and commit api request
+        when(storageManager.stageBlockAsync(anyString(), anyString(), any()))
+                .thenReturn(Mono.empty());
+
         outputStream.write(bytesToBeWritten, offset, length);
 
         verify(storageManager, times(3))
-                .append(any(), any());
+                .stageBlockAsync(anyString(), anyString(), any());
 
-        Assertions.assertEquals(9, outputStream.getPos());
+        assertEquals(9, outputStream.getPos());
     }
 
+    /**
+     * <b>Method: {@link ParquetOutputStream#write(byte[], int, int)}</b>.<br>
+     * <b>Assumptions: </b>
+     * <ul>
+     *     <li>Commit flag set to true</li>
+     *     <li>Non-empty buffer</li>
+     * </ul>
+     *
+     * <p><b>Expectations: </b>
+     * <ul>
+     *     <li>Should invoke {@link StorageManager}</li>
+     * </ul>
+     */
     @Test
     @DisplayName("Given commit flag set to true and non-empty buffer, close method should invoke storage manager")
     void close_givenCommitFlagTrueAndBufferNotEmpty_shouldInvokeStorageManager() throws IOException {
@@ -181,12 +299,17 @@ public class ParquetOutputStreamTest {
         String blobName = "test-blob";
         int partSize = 3;
 
-        storageManager = mock(StorageManager.class);
         ParquetOutputStream outputStream = new ParquetOutputStream(storageManager, blobName, partSize);
 
         byte[] bytesToBeWritten = {1, 2};
         int offset = 0;
         int length = 2;
+
+        // Mocking staging and commit api request
+        when(storageManager.stageBlockAsync(anyString(), anyString(), any()))
+                .thenReturn(Mono.empty());
+        when(storageManager.commitBlockIdsAsync(anyString(), any(), anyBoolean()))
+                .thenReturn(Mono.empty());
 
         /*
         write method will not invoke the storage manager as the buffer size is not full yet.
@@ -201,7 +324,11 @@ public class ParquetOutputStreamTest {
          */
         outputStream.close();
 
+        // 1 for staging & 1 for commit
         verify(storageManager, times(1))
-                .append(any(), any());
+                .stageBlockAsync(anyString(), anyString(), any());
+
+        verify(storageManager, times(1))
+                .commitBlockIdsAsync(anyString(), any(), anyBoolean());
     }
 }
